45654d8ae7e52a1eb74c2f55dea98ac72f87d9cc
Alexander Medvedev <razerguy999@gmail.com>
2024-02-10 11:26:51 +0200

    Deleted unnecessary comment characters

--- src/test/java/org/example/CalculatorAppTest.java
+++ src/test/java/org/example/CalculatorAppTest.java
@@ -9,56 +9,56 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CalculatorAppTest {
 
     @Test
-    public void sumOfTwoIntegers() {   //
+    public void sumOfTwoIntegers() {
         Calculator calculator = new Calculator();
         int result = calculator.sumOfTwoIntegers(2,5);
         assertEquals(7,result);
     }
 
     @Test
-    public void sumOfTwoNegativeNumbers () { //
+    public void sumOfTwoNegativeNumbers () {
         Calculator calculator = new Calculator();
         int result = calculator.sumOfTwoIntegers(-3,-5);
         assertEquals(-8,result);
      }
 
     @Test
-    public void sumOfTwoNumbers () {  //
+    public void sumOfTwoNumbers () {
         Calculator calculator = new Calculator();
         int result = calculator.sumOfTwoIntegers(10,0);
         assertEquals(10, result);
     }
 
     @Test
-    public void sumOfNegativeAndPositiveNumbers () {   //
+    public void sumOfNegativeAndPositiveNumbers () {
         Calculator calculator = new Calculator();
         int result = calculator.sumOfTwoIntegers(-4,8);
         assertEquals(4,result);
     }
 
     @Test
-    public void subtractionOfTwoNumbers() {  //
+    public void subtractionOfTwoNumbers() {
         Calculator calculator = new Calculator();
         int result = calculator.subtractingOfTwoIntegers(10,5);
         assertEquals (5,result);
     }
 
     @Test
-    public void subtractionOfTwoEqualNumbers() { //
+    public void subtractionOfTwoEqualNumbers() {
         Calculator calculator = new Calculator();
         int result = calculator.subtractingOfTwoIntegers(10,10);
         assertEquals(0,result);
     }
 
     @Test
-    public void subtractionOfBiggerAndZeroValues() { //
+    public void subtractionOfBiggerAndZeroValues() {
         Calculator calculator = new Calculator();
         int result = calculator.subtractingOfTwoIntegers(10,0);
         assertEquals(10,result);
     }
 
     @Test
-    public void subtractionBiggerOfSmallNum() { //
+    public void subtractionBiggerOfSmallNum() {
         Calculator calculator = new Calculator();
         int result  = calculator.subtractingOfTwoIntegers(5,10);
         assertEquals(-5, result);
@@ -66,14 +66,14 @@ public class CalculatorAppTest {
 
 
     @Test
-    public void firstNumIsSmallerThanSecond () { //
+    public void firstNumIsSmallerThanSecond () {
         Calculator calculator = new Calculator();
         boolean result = calculator.firstNumIsSmallerThanSecondNum(10,12);
         assertTrue(result);
     }
 
     @Test
-    public void secondNumIsBiggerThanFirst () { //
+    public void secondNumIsBiggerThanFirst () {
         Calculator calculator = new Calculator();
         boolean result = calculator.secondNumIsBiggerThanFirstNum(2,1);
         assertTrue(result);
